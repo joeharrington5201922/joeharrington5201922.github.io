3cd02a81eea27a110d747a1afaa542f68c1a0cbb
emallickhossain <emallickhossain@gmail.com>
2018-09-10 19:26:03 -0400

    Update cv, books, CRESSE. Add Bloomberg, hub and spoke, and updated other papers

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 	}).mouseout(function(){
 		$(this).attr('src',"images/joeHarrington.jpg");
 	});
-	
+
 	setInterval(
 		function(){$('.bodySection .details .flashNews').animate({'left':'-700px'},7500,function(){$(this).css('left','390px')});}
 	);
-	
+
 	$('.bodySection').css('height',($(this).height()-70)+"px");
 	//$('.homePageContent').css('height',($(this).height()-100)+"px");
 	$(window).scroll(function(){
@@ -17,18 +17,21 @@ $(document).ready(function(){
 		else
 			$('.tabsSection').css('top','0px');
 	});
-	
+
 	$('.tabsSection').height(window.innerHeight);
 	$('.tabsContainer').height($('.tabsSection').height()-$('.tabs').height());
 	$('.goToTop').click(function(){
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 	})
-	
-	$('.book1, .book2, .booksDescription').css('height',$('.booksPage .book1').width()+100+"px");
+
+	$('.book1, .book2, .book3, .booksDescription').css('height',$('.booksPage .book1').width()+100+"px");
 	$('.book1 img').click(function(){
-		window.open('http://mitpress.mit.edu/books/economics-regulation-and-antitrust?ttype=2&tid=10586');
+		window.open('https://mitpress.mit.edu/books/economics-regulation-and-antitrust-fifth-edition');
 	});
 	$('.book2 img').click(function(){
-		window.open('http://www.worthpublishers.com/harringtonpreview/');
+		window.open('https://macmillanlearning.com/Catalog/product/gamesstrategiesanddecisionmaking-secondedition-harringtonjr');
+	});
+	$('.book3 img').click(function(){
+		window.open('https://mitpress.mit.edu/books/theory-collusion-and-competition-policy');
 	});
 });
