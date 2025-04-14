bb61cd39725ddf0a559e6342348bbc1608c98a25
joeharrington5201922 <41289678+joeharrington5201922@users.noreply.github.com>
2025-04-14 09:51:38 -0400

    updated style and script for new book

--- js/script.js
+++ js/script.js
@@ -24,17 +24,20 @@ $(document).ready(function(){
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 	})
 
-	$('.book1, .book2, .book3, .book4, .booksDescription').css('height',$('.booksPage .book1').width()+50+"px");
-	$('.book1 img').click(function(){
+	$('.book1, .book2, .book3, .book4, .book5, .booksDescription').css('height',$('.booksPage .book1').width()+50+"px");
+	$('.book3 img').click(function(){
 		window.open('https://mitpress.mit.edu/books/economics-regulation-and-antitrust-fifth-edition');
 	});
-	$('.book2 img').click(function(){
+	$('.book5 img').click(function(){
 		window.open('https://macmillanlearning.com/Catalog/product/gamesstrategiesanddecisionmaking-secondedition-harringtonjr');
 	});
-	$('.book3 img').click(function(){
+	$('.book4 img').click(function(){
 		window.open('https://mitpress.mit.edu/books/theory-collusion-and-competition-policy');
 	});
-	$('.book4 img').click(function(){
+	$('.book2 img').click(function(){
 		window.open('https://mitpress.mit.edu/books/hub-and-spoke-cartels');
+	});
+	$('.book1 img').click(function(){
+		window.open('https://www.cambridge.org/gb/universitypress/subjects/economics/industrial-economics/cartels-diagnosed-new-insights-collusion?format=HB&isbn=9781009428484');
 	});
 });
